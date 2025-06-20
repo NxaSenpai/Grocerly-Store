e9a8f1d7a26f06572c70717b4618e28c75941ee8
Tang Nakry <nxasenpai007@gmail.com>
2025-06-21 00:40:40 +0700

    almost

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
-// tailwind.config.js 
+const params = new URLSearchParams(window.location.search);
+const category = params.get("category");
 
-module.exports = {
-    content: [],
-    theme: {
-      extend: {
-        backgroundImage: theme => ({
-          'gradient-to-45': 
-              'linear-gradient(45deg, #ffed4a, #ff3860)',
-          'gradient-to-135': 
-              'linear-gradient(135deg, #ffed4a, #ff3860)',
-          // You can add more custom classes here
-        })
-      },
-    },
-    plugins: [],
+document.querySelectorAll('.item').forEach(item => {
+  if (item.classList.contains(category)) {
+    item.classList.remove('hidden');
+  } else {
+    item.classList.add('hidden');
   }
-
-  bg-gradient-to-b from-green-600 to-yellow-800;
+});
